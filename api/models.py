88da95c122246a1c5a6189413dbfbc74b9bb05f1
Marcelo Ruys Rodrigues <ruysmarcelo@gmail.com>
2024-05-09 09:56:48 -0300

    first commit

---
+++ api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import uuid
+
+def upload_image(instance, filename):
+    return f"{instance(str(uuid.uuid4()))}-{filename}"
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    rua = models.CharField(max_length=255)
+    bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+    uf = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    cep = models.CharField(max_length=255)
+    numero = models.CharField(max_length=255)
+    image = models.ImageField(upload_to=upload_image, blank=True, null=True)
+
